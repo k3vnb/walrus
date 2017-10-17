89ffe1fa2afee396295b2df849037916fbee5f51
John Sawiris and Kevin Boyle <john@>
2017-10-17 09:23:12 -0700

    add second html page, link to page, add js for .addClass and .removeClass functions, add css background colors

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,19 @@ $(document).ready(function() {
   $(".reveal").click(function() {
     $("#cereal-showing").fadeToggle(1000);
   });
+  //colors page
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
 });
